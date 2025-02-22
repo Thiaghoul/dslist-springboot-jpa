81b250f12a00c113c21b7ca518a4790e7bb35f31
thiaghoul <thiagohnunes.w@gmail.com>
2025-02-22 19:09:03 -0300

    Project Created

---
+++ src/test/java/com/thiaghoul/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiaghoul.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
